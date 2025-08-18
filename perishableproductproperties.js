34ae2a87d5bddc701e921d027a6cbc3b6e9cc8c9
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 19:49:10 -0400

    Added constructor to PerishableProductProperties

--- perishableproductproperties.js
+++ perishableproductproperties.js
@@ -1,6 +1,13 @@
 /**
  * Represents a product that can expire.
  */
-class PerishableProductProperties {
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        // 'super()' calls the constructor of the parent class.
+        super(name, price, quantity);
+
+        // Initialize the new property.
+        this.expirationDate = expirationDate;
+    }
 
 }
