5aa1278c6155070ae2c0fabbde980b92415a6932
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 19:50:03 -0400

    Overridden toString method in PerishableProductProperties

--- perishableproductproperties.js
+++ perishableproductproperties.js
@@ -9,5 +9,11 @@ class PerishableProductProperties extends ProductProperties {
         // Initialize the new property.
         this.expirationDate = expirationDate;
     }
-
+    /**
+     * Overrides the parent's toString method to include the expiration date.
+     * @returns {string} A formatted string with perishable product details.
+     */
+    toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+    }
 }
