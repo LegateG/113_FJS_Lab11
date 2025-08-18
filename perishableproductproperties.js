1b84b4eeffb5d15897d93af5b79106924ff4390c
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 19:51:38 -0400

    Added isExpired method to PerishableProductProperties

--- perishableproductproperties.js
+++ perishableproductproperties.js
@@ -16,4 +16,18 @@ class PerishableProductProperties extends ProductProperties {
     toString() {
         return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
     }
+        /**
+     * Checks if the product is expired based on the current date.
+     * @returns {boolean} True if the product is expired, false otherwise.
+     */
+    isExpired() {
+        // Get the current date and time.
+        const today = new Date();
+        // Create a Date object from the expirationDate string.
+        const expiryDate = new Date(this.expirationDate);
+        // Set the time of today to 00:00:00 to compare dates only.
+        today.setHours(0, 0, 0, 0);
+        // Return true if the expiration date is before today.
+        return expiryDate < today;
+    }
 }
