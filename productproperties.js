f887ef7113edef3d2c938af25ead2de2ab0ee5a3
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 19:25:51 -0400

    Added constructor to ProductProperties class

--- productproperties.js
+++ productproperties.js
@@ -2,5 +2,11 @@
  * Represents a basic product with common properties.
  */
 class ProductProperties {
+    // The constructor initializes an object instance of a class.
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
 }
