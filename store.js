1a576186082c90a2c43f5894925fbc9522489c3f
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 20:49:23 -0400

    Added addProduct method to Store class

--- store.js
+++ store.js
@@ -6,4 +6,11 @@ class Store {
         // Initializes the inventory as an empty array.
         this.inventory = [];
     }
+    /**
+     * Adds a product to the store's inventory.
+     * @param {ProductProperties} product - The product instance to add.
+     */
+    addProduct(product) {
+        this.inventory.push(product);
+    }
 }
