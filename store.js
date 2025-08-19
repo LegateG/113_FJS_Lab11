8da5d99814f0b37837f03c8163dfde859f213ff2
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 20:48:01 -0400

    Added constructor to Store class

--- store.js
+++ store.js
@@ -2,5 +2,8 @@
  * Manages the inventory of products for a store.
  */
 class Store {
-
+    constructor() {
+        // Initializes the inventory as an empty array.
+        this.inventory = [];
+    }
 }
