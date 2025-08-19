fbef7d87134179f56546d1467d809e288b4b65bc
LegateG <gorkemb@hotmail.com.tr>
2025-08-18 20:55:05 -0400

    Added getInventoryValue method to Store class

--- store.js
+++ store.js
@@ -13,4 +13,11 @@ class Store {
     addProduct(product) {
         this.inventory.push(product);
     }
+    /**
+     * Calculates the total value of all products in the inventory.
+     * @returns {number} The total value of the inventory.
+     */
+    getInventoryValue() {
+        return this.inventory.reduce((total, product) => total + product.getTotalValue(), 0);
+    }
 }
